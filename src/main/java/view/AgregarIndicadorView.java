1877676656af3b3c4e1ac8e8796cf35b0c3ecd50
DavidGeorge <jorgetitomamani@est.frba.utn.edu.ar>
2017-05-30 22:39:00 -0300

    arreglos en las vistas

--- src/main/java/view/AgregarIndicadorView.java
+++ src/main/java/view/AgregarIndicadorView.java
@@ -21,18 +21,20 @@ public class AgregarIndicadorView extends Dialog<AgregarIndicadorViewM> {
 	protected void createFormPanel(Panel mainPanel) {
 		Panel form = new Panel(mainPanel);
 		this.setTitle("Agregar Indicador");
-		form.setLayout(new ColumnLayout(4));
+		form.setLayout(new ColumnLayout(2));
 
-		new Label(form).setText("\t\tNombre: ");
+		new Label(form).setText("\t\t\t\tNombre:");
 		new TextBox(form).setWidth(250).bindValueToProperty("nombre");
 
-		new Label(form).setText("\t\tFormula: ");
+		new Label(form).setText("\t\t\t\tFormula:");
 		new TextBox(form).setWidth(250).bindValueToProperty("formula");
 
 	}
 
 	@Override
 	protected void addActions(Panel actions) {
+
+		new Label(actions).setText("\t\t\t\t\t");
 		new Button(actions).setCaption("Guardar")
 				.onClick(this::guardarIndicador).setAsDefault().setWidth(140);
 		new Button(actions).setCaption("Cancelar").onClick(this::cancel)
