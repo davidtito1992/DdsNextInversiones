c2d798f2fdac6307921f89e62fe72a96f7344d22
camigil <camilabgil@hotmail.com>
2017-06-02 18:52:11 -0300

    test

--- src/test/java/domaintest/ValidarIndicadorTest.java
+++ src/test/java/domaintest/ValidarIndicadorTest.java
@@ -9,6 +9,8 @@ import model.Indicador;
 import org.junit.Before;
 import org.junit.Test;
 
+import app.AppData;
+
 public class ValidarIndicadorTest {
 	Indicador indicadorSinNombre;
 	Indicador indicadorCompuestoDeNumeros;
@@ -16,62 +18,43 @@ public class ValidarIndicadorTest {
 	Indicador indicadorNuevo;
 	Indicador indicadorConNombreYaExistente;
 	ArrayList<Indicador> repoIndicadores;
-	
+
 	@Before
 	public void initialize() {
-		indicadorSinNombre =  new Indicador(null,null);
-		indicadorCompuestoDeNumeros = new Indicador("IndicadorCompuestoDeNumeros","45 45 45 45");
-		indicadorNombreContieneEspacios = new Indicador("Nombre con espacios", "2+2");
-		indicadorNuevo = new Indicador("IndicadorNuevo","2*2"); 
-		indicadorConNombreYaExistente = new Indicador("IndicadorNuevo","2*3");
-//		repoIndicadores = new ArrayList<Indicador>();
-//		repoIndicadores.add(indicadorNuevo);
+		AppData app = new AppData();
+		indicadorSinNombre = new Indicador(null, null);
+		indicadorCompuestoDeNumeros = new Indicador(
+				"IndicadorCompuestoDeNumeros", "45 45 45 45");
+		indicadorNombreContieneEspacios = new Indicador("Nombre con espacios",
+				"2+2");
+		indicadorNuevo = new Indicador("IndicadorNuevo", "2*2");
+		indicadorConNombreYaExistente = new Indicador("IndicadorNuevo", "2*3");
 		indicadorNuevo = new Indicador("IndicadorNuevo", "IndicadorNuevo + 3");
 	}
-	
-//	@Test 
-//	public void indicadorSinNombreTest(){
-//		try{
-//			indicadorSinNombre.guardarIndicador();
-//		}catch(Exception e){
-//			assertEquals("Debe ingresar nombre y formula para guardar correctamente. Intentelo nuevamente", e.getMessage());
-//		}
-//	}
-//
-//	@Test
-//	public void indicadorCompuestoSoloDeNumeros(){
-//		try{
-//			indicadorCompuestoDeNumeros.guardarIndicador();
-//		}catch(Exception e){
-//			assertEquals("La formula no puede estar compuesta por numeros unicamente", e.getMessage());
-//		}
-//	}
-//	
-//	@Test
-//	public void indicadorNoDebeContenerEspacios(){
-//		try{
-//			indicadorNombreContieneEspacios.guardarIndicador();
-//		}catch(Exception e){
-//			assertEquals("Favor de ingresar un nombre que no contenga espacios", e.getMessage());
-//		}
-//	}
-//	
-////	@Test 
-////	public void indicadorConNombreYaExistente(){
-////		try{
-////			indicadorConNombreYaExistente.guardarIndicador();
-////		}catch(Exception e){
-////			assertEquals("Un indicador con ese nombre ya se encuentra cargado en el sistema, Intentelo nuevamente", e.getMessage());
-////		}
-////	}
-//	
-//	@Test
-//	public void testIndicadorConNombreExistenteEnLaFormula(){
-//		try{
-//			indicadorNuevo.guardarIndicador();
-//		}catch(Exception e){
-//			assertEquals("No puede usar ese nombre porque se encuentra dentro de la formula del mismo", e.getMessage());
-//		}
-//	}
-	
+
+	@Test
+	public void indicadorCompuestoSoloDeNumeros() {
+		try {
+			AppData app = new AppData();
+			app.guardarIndicador(indicadorCompuestoDeNumeros);
+		} catch (Exception e) {
+			assertEquals(
+					"La formula no puede estar compuesta por numeros unicamente",
+					e.getMessage());
+		}
+	}
+
+	@Test
+	public void indicadorNoDebeContenerEspacios() {
+		try {
+			AppData app = new AppData();
+			app.guardarIndicador(indicadorNombreContieneEspacios);
+		} catch (Exception e) {
+			assertEquals(
+					"Favor de ingresar un nombre que no contenga espacios",
+					e.getMessage());
+		}
+	}
+
+
 }
