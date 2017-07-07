b2ecee04cb7ffa01a3e2a8d4defacc092a7575b2
romanferro <romanferro22@gmail.com>
2017-07-07 04:05:14 -0300

    Comparador de periodos para crecientes y decrecientes

--- src/main/java/condiciones/CondicionTaxativaDecreciente.java
+++ src/main/java/condiciones/CondicionTaxativaDecreciente.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 import app.DslIndicador;
 import model.Empresa;
 import model.Periodo;
+import model.PeriodoComparator;
 import model.RegistroIndicador;
 import parserIndicador.ParseException;
 
@@ -18,7 +19,9 @@ public class CondicionTaxativaDecreciente implements CondicionTaxativa{
 
 	public boolean calcular(Empresa empresa) throws ParseException {
 		List<Periodo> periodos = empresa.getPeriodos().stream()
-		.filter(periodo -> periodo.getAnio().getValue() > 2017-ultimosAnios).collect(Collectors.toList());
+		.filter(periodo -> periodo.getAnio().getValue() > 2017-ultimosAnios)
+		.sorted(new PeriodoComparator())
+		.collect(Collectors.toList());
 		for (int i = 0; i < periodos.size()-1; i++) {
 			if(aplicarIndicador(indicador, empresa.getNombre(), periodos.get(i).getAnio(),periodos.get(i).getSemestre()).doubleValue() 
 					< aplicarIndicador(indicador, empresa.getNombre(), periodos.get(i+1).getAnio(),periodos.get(i+1).getSemestre()).doubleValue()){
