8a3c68a73f23113d18e974875cfd364eca7c917d
agusaaa <agus.sicsic@hotmail.com>
2017-07-07 03:23:59 -0300

    condicion cualitativa sumatoria

---
+++ src/main/java/condiciones/CondicionCualitativaSumatoria.java
@@ -0,0 +1,39 @@
+package condiciones;
+
+import java.math.BigDecimal;
+import java.time.Year;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import app.DslIndicador;
+import model.Empresa;
+import model.Periodo;
+import model.RegistroIndicador;
+import parserIndicador.ParseException;
+
+public class CondicionCualitativaSumatoria implements CondicionCualitativa{
+	private RegistroIndicador indicador;
+	private int ultimosAnios;
+	
+	public CondicionCualitativaSumatoria(RegistroIndicador indicador,int ultimosAnios){
+		this.indicador = indicador;
+		this.ultimosAnios = ultimosAnios;
+	}
+	
+	public double calcular(Empresa empresa) throws ParseException{
+		BigDecimal acumulador = BigDecimal.ZERO;
+		List<Periodo> periodos = empresa.getPeriodos().stream()
+		.filter(periodo -> periodo.getAnio().getValue() > 2017-ultimosAnios).collect(Collectors.toList());
+		for (int i = 0; i < periodos.size(); i++) {
+			acumulador = acumulador.add(aplicarIndicador(indicador, empresa.getNombre(), periodos.get(i).getAnio(),periodos.get(i).getSemestre()));
+		}
+		
+		return acumulador.doubleValue();
+	}
+	
+	
+	private BigDecimal aplicarIndicador(RegistroIndicador indicador, String nombreEmpresa, Year anio, int semestre) throws ParseException{
+		return new DslIndicador().prepararFormula(indicador, nombreEmpresa, anio, semestre).calcular();
+	}
+	
+}
