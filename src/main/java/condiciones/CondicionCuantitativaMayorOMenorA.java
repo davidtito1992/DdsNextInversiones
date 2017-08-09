3d121dd513f7c040046161fcfdefb3833f644bb3
romanferro <romanferro22@gmail.com>
2017-08-09 03:03:41 -0300

    Metodo repetido en CondicionCuantitativaMayorA eliminado

--- src/main/java/condiciones/CondicionCuantitativaMayorOMenorA.java
+++ src/main/java/condiciones/CondicionCuantitativaMayorOMenorA.java
@@ -20,18 +20,6 @@ public class CondicionCuantitativaMayorOMenorA extends CondicionSumatoria {
 		this.peso = peso;
 	}
 
-	public BigDecimal sumador(Empresa empresa) throws ParseException {
-		List<Periodo> periodos = super.periodosDesdexAnio(empresa);
-		BigDecimal acumulador = BigDecimal.ZERO;
-
-		for (int i = 0; i < periodos.size(); i++) {
-			acumulador = acumulador.add(super.aplicarIndicador(indicador,
-					empresa.getNombre(), periodos.get(i).getAnio(), periodos
-							.get(i).getSemestre()));
-		}
-		return acumulador;
-	}
-
 	@Override
 	public RankingEmpresa calcular(RankingEmpresa rEmpresa) throws ParseException {
 		BigDecimal nuevoValor = sumador(rEmpresa.getEmpresa()).multiply(new BigDecimal(peso)).multiply(multiplicador());
