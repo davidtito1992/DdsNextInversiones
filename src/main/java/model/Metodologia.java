b0b90de0db34c8e7232539b5603684613d0d9926
romanferro <romanferro22@gmail.com>
2017-08-09 01:19:15 -0300

    Bread and butter de metodologias

--- src/main/java/model/Metodologia.java
+++ src/main/java/model/Metodologia.java
@@ -1,27 +1,23 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.uqbar.commons.model.Entity;
 import org.uqbar.commons.utils.Observable;
 
+import parserIndicador.ParseException;
+import RankingEmpresa.RankingEmpresa;
 import condiciones.Condicion;
 
 @SuppressWarnings("serial")
 @Observable
 public class Metodologia extends Entity {
 
-	// public Metodologia(String nombre, List<CondicionTaxativa> condTaxativas,
-	// List<CondicionCualitativa> condCualitativas) {
-	// this.nombre = nombre;
-	// this.condicionesTaxativas = condTaxativas;
-	// this.condicionesCualitativas = condCualitativas;
-	// }
 	public Metodologia(String nombre,
 			ArrayList<Condicion> condiciones) {
 		this.nombre = nombre;
-		// this.condicionesTaxativas = condTaxativas;
-		// this.condicionesCualitativas = condCualitativas;
 		this.condiciones = condiciones;
 
 	}
@@ -49,8 +45,22 @@ public class Metodologia extends Entity {
 			ArrayList<Condicion> condiciones) {
 		this.condiciones = condiciones;
 	}
-	public void calcular() {
 
+	private RankingEmpresa calcularEmpresa(RankingEmpresa rEmpresa){
+		try{
+			for (Condicion condicion : condiciones) {
+			    rEmpresa = condicion.calcular(rEmpresa);
+			}
+		}catch (RuntimeException e){
+			//Esto es cuando una taxativa no pasa
+		}catch (ParseException e){
+			//Esto es cuando un indicador no se puede correr
+		}
+		return rEmpresa;
+	}
+	
+	public List<RankingEmpresa> calcularEmpresas(ArrayList<RankingEmpresa> rEmpresas) {
+		return rEmpresas.stream().map(empresa->calcularEmpresa(empresa)).collect(Collectors.toList());
 	}
 
 
