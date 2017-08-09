a12a2e6e9efb139b70a87e15e5886c8ac591d8e1
romanferro <romanferro22@gmail.com>
2017-08-09 03:41:37 -0300

    Tests de condiciones MayorA

--- src/test/java/domaintest/ControladorDeMetodologiaTest.java
+++ src/test/java/domaintest/ControladorDeMetodologiaTest.java
@@ -10,8 +10,8 @@ import model.RegistroIndicador;
 import org.junit.Assert;
 import org.junit.Test;
 
-import app.AppData;
 import RankingEmpresa.RankingEmpresa;
+import app.AppData;
 import condiciones.Condicion;
 import condiciones.CondicionCuantitativaMayorOMenorA;
 import condiciones.CondicionSumatoria.Criterio;
@@ -20,24 +20,35 @@ import condiciones.CondicionTaxativaMayorOMenorA;
 public class ControladorDeMetodologiaTest {
 
 	@Test
-	public void getIndicadoresCondionadosOrdenadosTest() throws Exception {
+	public void metodologiaConCondicionCuantitativaMayorATest() throws Exception {
 		new AppData().cargarEmpresas();
 		new AppData().cargarIndicadores();
 		
 		ArrayList<Condicion> condicionesPrueba = new ArrayList<Condicion>();
-		
 		RegistroIndicador ingresoNeto = new AppData().getRepositorioIndicadores().getRegistroIndicador("IngresoNeto");
 		Empresa facebook = new AppData().getRepositorioEmpresas().getEmpresa("Facebook");
 		RankingEmpresa rEmpresa = new RankingEmpresa(BigDecimal.ZERO, facebook);
 		
-		condicionesPrueba.add(new CondicionTaxativaMayorOMenorA(Criterio.mayorA, ingresoNeto, 2, BigDecimal.valueOf(5)));
-		condicionesPrueba.add(new CondicionCuantitativaMayorOMenorA(condiciones.CondicionCuantitativaMayorOMenorA.Criterio.mayorA,ingresoNeto,2,1));
-		
+		condicionesPrueba.add(new CondicionCuantitativaMayorOMenorA(Criterio.mayorA,ingresoNeto,2,1));
 		Metodologia metodologia = new Metodologia("Metodlogia Prueba",condicionesPrueba);
-
-		Assert.assertEquals(0,BigDecimal.valueOf(16).compareTo(metodologia.calcularEmpresa(rEmpresa).getRanking()));
-				
-
+		
+		Assert.assertEquals(0,BigDecimal.valueOf(16).compareTo(metodologia.calcularEmpresa(rEmpresa).getRanking()));		
+	}
+	
+	@Test
+	public void metodologiaConCondicionTaxativaMayorATest() throws Exception {
+		new AppData().cargarEmpresas();
+		new AppData().cargarIndicadores();
+		
+		ArrayList<Condicion> condicionesPrueba = new ArrayList<Condicion>();
+		RegistroIndicador ingresoNeto = new AppData().getRepositorioIndicadores().getRegistroIndicador("IngresoNeto");
+		Empresa facebook = new AppData().getRepositorioEmpresas().getEmpresa("Facebook");
+		RankingEmpresa rEmpresa = new RankingEmpresa(BigDecimal.ZERO, facebook);
+		
+		condicionesPrueba.add(new CondicionTaxativaMayorOMenorA(Criterio.mayorA,ingresoNeto,2,BigDecimal.valueOf(17)));
+		Metodologia metodologia = new Metodologia("Metodlogia Prueba",condicionesPrueba);
+		
+		Assert.assertTrue(metodologia.calcularEmpresa(rEmpresa).getErrorTaxativa());	
 	}
 
 }
