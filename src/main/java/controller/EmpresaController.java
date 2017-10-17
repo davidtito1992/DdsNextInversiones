cf7bc44fa008470b63c6204d1ae20900522d914d
agusaaa <agus.sicsic@hotmail.com>
2017-10-17 01:51:46 -0300

    combos llenos con valores

--- src/main/java/controller/EmpresaController.java
+++ src/main/java/controller/EmpresaController.java
@@ -19,16 +19,32 @@ public class EmpresaController extends Controller{
 	private static EmpresaViewM adapter = new EmpresaViewM();
 	
 	public static ModelAndView home(Request req, Response res) {
-		
-			HashMap<String, List<SnapshotEmpresa>> mapEmpresas = new HashMap<>();
-			RepositorioEmpresa repo = RepositorioEmpresa.getInstance();
-			
-			Long idUsuario = autenticar(req,res);
-			List<Empresa> empresasObtenidas = idUsuario != null ? repo.findFromUser(idUsuario) : new ArrayList<>();
-			List<SnapshotEmpresa> snaps = adapter
-					.dameSnapshotEmpresas(empresasObtenidas);
-			mapEmpresas.put("empresas", snaps);
-			
-			return new ModelAndView(mapEmpresas, "homePage/empresas.hbs");
+
+		RepositorioEmpresa repo = RepositorioEmpresa.getInstance();
+
+		Long idUsuario = autenticar(req, res);
+		List<Empresa> empresasObtenidas = idUsuario != null ? repo
+				.findFromUser(idUsuario) : new ArrayList<>();
+
+		return new ModelAndView(armarHashMap(empresasObtenidas),
+				"homePage/empresas.hbs");
 	}
+	
+	public static HashMap<String, List<?>> armarHashMap(List<Empresa> empresasObtenidas){
+		
+		HashMap<String, List<?>> mapEmpresas = new HashMap<>();
+		
+		mapEmpresas.put("empresas", adapter.dameSnapshotEmpresas(empresasObtenidas));
+		mapEmpresas.put("nombresCuentas", RepositorioEmpresa.getInstance().
+				todosLosNombresDeCuentas(empresasObtenidas));
+		mapEmpresas.put("nombresEmpresas", RepositorioEmpresa.getInstance().
+				todosLosNombresDeEmpresas(empresasObtenidas));
+		mapEmpresas.put("anios",RepositorioEmpresa.getInstance().
+				todosLosAnios(empresasObtenidas));
+		mapEmpresas.put("periodos",RepositorioEmpresa.getInstance().
+				todosLosPeriodos(empresasObtenidas));
+		
+		return mapEmpresas;
+	}
+	
 }
