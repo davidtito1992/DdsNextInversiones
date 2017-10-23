8c0e20816908737d5ee8d12850e5c9220458cc37
agusaaa <agus.sicsic@hotmail.com>
2017-10-23 12:22:57 -0300

    se sacan variables estaticas de EmpresaController

--- src/main/java/controller/EmpresaController.java
+++ src/main/java/controller/EmpresaController.java
@@ -14,37 +14,36 @@ import java.util.Objects;
 
 public class EmpresaController extends Controller {
 
-	static String nombreSeleccionado;
-	static String cuentaSeleccionada;
-	static Integer semestreSeleccionado;
-	static Year anioSeleccionado;
-	static RepositorioEmpresa repo = RepositorioEmpresa.getInstance();
-
 	public static ModelAndView home(Request req, Response res) {
-		cuentaSeleccionada = Objects.isNull(req.queryParams("nombreCuenta"))
+		String cuentaSeleccionada = Objects.isNull(req.queryParams("nombreCuenta"))
 				|| req.queryParams("nombreCuenta").isEmpty() ? null : req.queryParams("nombreCuenta");
-		nombreSeleccionado = Objects.isNull(req.queryParams("nombreEmpresa"))
+		String nombreSeleccionado = Objects.isNull(req.queryParams("nombreEmpresa"))
 				|| req.queryParams("nombreEmpresa").isEmpty() ? null : req.queryParams("nombreEmpresa");
-		anioSeleccionado = Objects.isNull(req.queryParams("anio")) || req.queryParams("anio").isEmpty() ? null
+		Year anioSeleccionado = Objects.isNull(req.queryParams("anio")) || req.queryParams("anio").isEmpty() ? null
 				: Year.parse(req.queryParams("anio"));
-		semestreSeleccionado = Objects.isNull(req.queryParams("semestre")) || req.queryParams("semestre").isEmpty()
+		Integer semestreSeleccionado = Objects.isNull(req.queryParams("semestre")) || req.queryParams("semestre").isEmpty()
 				? null
 				: Integer.parseInt(req.queryParams("semestre"));
+		
+		HashMap<String, Object> mapEmpresas = armarHashMap(
+				autenticar(req, res), cuentaSeleccionada, nombreSeleccionado,
+				anioSeleccionado, semestreSeleccionado);
 
-		return new ModelAndView(armarHashMap(autenticar(req, res)), "homePage/empresas.hbs");
+		return new ModelAndView(mapEmpresas, "homePage/empresas.hbs");
 	}
 
-	public static HashMap<String, Object> armarHashMap(Long idUsuario) {
-
+	public static HashMap<String, Object> armarHashMap(Long idUsuario,
+			String cuentaSeleccionada, String nombreSeleccionado,
+			Year anioSeleccionado, Integer semestreSeleccionado) {
 		HashMap<String, Object> mapEmpresas = new HashMap<>();
 
 		List<Empresa> todasLasEmpresasDelUsuario = RepositorioEmpresa.getInstance().allInstancesUser(idUsuario);
 		List<Empresa> empresasPorFiltros = idUsuario != null
-				? repo.filtrar(cuentaSeleccionada, nombreSeleccionado, semestreSeleccionado, anioSeleccionado,
+				? RepositorioEmpresa.getInstance().filtrar(cuentaSeleccionada, nombreSeleccionado, semestreSeleccionado, anioSeleccionado,
 						idUsuario)
 				: new ArrayList<>();
 
-		mapEmpresas.put("empresas", repo.dameSnapshotEmpresas(empresasPorFiltros, nombreSeleccionado,
+		mapEmpresas.put("empresas", RepositorioEmpresa.getInstance().dameSnapshotEmpresas(empresasPorFiltros, nombreSeleccionado,
 				cuentaSeleccionada, anioSeleccionado, semestreSeleccionado));
 		mapEmpresas.put("nombresCuentas",
 				RepositorioEmpresa.getInstance().todosLosNombresDeCuentas(todasLasEmpresasDelUsuario));
