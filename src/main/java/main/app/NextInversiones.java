60850cc073f61042ff7977fc1f8ddeffdcc115e7
DavidGeorge <jorgetitomamani@est.frba.utn.edu.ar>
2017-10-29 01:36:39 -0300

    un println en NextInversiones..jeje

--- src/main/java/main/app/NextInversiones.java
+++ src/main/java/main/app/NextInversiones.java
@@ -1,48 +1,26 @@
-//package main.app;
-//
-//import java.io.IOException;
-//import main.server.Server;
-//
-//import org.uqbar.arena.Application;
-//import org.uqbar.arena.windows.Window;
-////
-////public class NextInversiones {
-////
-////	public static void main(String[] args) throws IOException {
-////		AplicacionContexto.getInstance();
-////		AplicacionContexto.getInstance().getInstanceRepoEmpresas();
-////		AplicacionContexto.getInstance().getInstanceRepoIndicadores();
-////		AplicacionContexto.getInstance().getInstanceRepoMetodologias();
-////		AplicacionContexto.getInstance().getInstanceRepoUsuarios();
-////		
-////		new Server();
-////		Server.main();
-////		// new NextInversiones().start();
-////
-////	}
-////
-////	// @Override
-////	// protected Window<?> createMainWindow() {
-////	// AplicacionContexto.getInstance().getInstanceRepoEmpresas();
-////	// AplicacionContexto.getInstance().getInstanceRepoIndicadores();
-////	// AplicacionContexto.getInstance().getInstanceRepoMetodologias();
-////	//
-////	// //return new MainView(this);
-////	// }
-////}
-//-
-//-public class NextInversiones extends Application {
-//-
-//-	public static void main(String[] args) {
-//-		AplicacionContexto.getInstance();
-//-		new NextInversiones().start();
-//-	}
-//-
-//-	@Override
-//-	protected Window<?> createMainWindow() {
-//-		AplicacionContexto.getInstance().getInstanceRepoEmpresas();
-//-		AplicacionContexto.getInstance().getInstanceRepoIndicadores();
-//-		AplicacionContexto.getInstance().getInstanceRepoMetodologias();
-//-		return new MainView(this);
-//-	}
-//-}
+package main.app;
+
+import java.io.IOException;
+
+public class NextInversiones {
+
+	public static void main(String[] args) throws IOException {
+		// // AplicacionContexto.getInstance();
+		// // AplicacionContexto.getInstance().getInstanceRepoEmpresas();
+		// // AplicacionContexto.getInstance().getInstanceRepoIndicadores();
+		// // AplicacionContexto.getInstance().getInstanceRepoMetodologias();
+		// // AplicacionContexto.getInstance().getInstanceRepoUsuarios();
+		// //
+		// new NextInversiones().start();
+        System.out.println("Inicia en modo Servidor!!!\nAlguien borro las vistas de arena!!\nDespues lo rastreamos ;)");
+	}
+	// //
+	// // // @Override
+	// // // protected Window<?> createMainWindow() {
+	// // // AplicacionContexto.getInstance().getInstanceRepoEmpresas();
+	// // // AplicacionContexto.getInstance().getInstanceRepoIndicadores();
+	// // // AplicacionContexto.getInstance().getInstanceRepoMetodologias();
+	// // //
+	// // // //return new MainView(this);
+	// // // }
+}
