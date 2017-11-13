219618232fd7edb04c08eaeb30475693e6a638fa
agusaaa <agus.sicsic@hotmail.com>
2017-11-13 17:03:58 -0300

    correcciones de codigo

--- DataFileManagment/src/main/app/DataFileManagement.java
+++ DataFileManagment/src/main/app/DataFileManagement.java
@@ -1,28 +1,15 @@
 package main.app;
 
-import java.util.ArrayList;
-
-import main.dataManagment.dataLoader.DataLoader;
-import main.dataManagment.dataLoader.DataLoaderFactory;
+import main.dataManagment.dataLoader.FileLoader;
 import main.repositories.RepositorioEmpresa;
-import model.Empresa;
 
 public class DataFileManagement {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Iniciando programa...");
-		new DataFileManagement().cargarEmpresas();
+		
+		RepositorioEmpresa repoEmpresa = RepositorioEmpresa.getInstance();
+		repoEmpresa.cargarListaDeElementos(new FileLoader().getDataEmpresas());
 	}
 	
-	public void cargarEmpresas() throws Exception {
-		// LEO ARCHIVO YA ADAPTADO
-		DataLoader cargador = DataLoaderFactory
-				.cargarData(DataLoaderFactory.ARCHIVO);
-		ArrayList<Empresa> empresas = cargador.getDataEmpresas();
-		this.getRepositorioEmpresas().cargarListaDeElementos(empresas);
-	}
-	
-	public RepositorioEmpresa getRepositorioEmpresas() {
-		return AplicacionContexto.getInstance().getInstanceRepoEmpresas();
-	}
 
 }
